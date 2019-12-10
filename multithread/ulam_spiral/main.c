0c876acfcb0ba23107e2106b55ddd3ebd3dc6e7e
Kaaame <ppatynowski@gmail.com>
2019-12-10 16:40:16 +0000

    16 nested threads done

--- multithread/ulam_spiral/main.c
+++ multithread/ulam_spiral/main.c
@@ -10,7 +10,10 @@
 #define MAX_NUM_OF_THREADS 16
 #define max(x,y) ((x) > (y) ? (x) : (y))
 
+#define NR_OF_THREADS 4
+
 #define _COUNT_LOOPS_
+#define NESTED
 
 FILE * fp;
 
@@ -21,7 +24,7 @@ int values[SIZE][SIZE];
 int isprime(int, int *);
 int ulam_get_map(int, int, int);
 
-static unsigned char thread_colors[MAX_NUM_OF_THREADS][3] =
+static unsigned char thread_colors[MAX_NUM_OF_THREADS + 1][3] =
 {
 	{0,0,0},
 	{0,0,255},
@@ -31,7 +34,15 @@ static unsigned char thread_colors[MAX_NUM_OF_THREADS][3] =
 	{225,248,114},
 	{206,60,16},
 	{29,137,2},
-	{255,255,255}
+	{255,255,255},
+	{23,122,255},
+	{132,255,4},
+	{255, 48, 20},
+	{112, 20, 119},
+	{200, 70, 70},
+	{7, 206, 46},
+	{44,137,70},
+	{255, 30 ,111}
 };
 
 int main()
@@ -76,39 +87,100 @@ int main()
 		j_end = j_start + BLOCK_SIZE(SIZE);
 		printf("ptid = %d\ti_start = %d\ti_end = %d\tj_start = %d\tj_end = %d\n",
 				ptid, i_start, i_end, j_start, j_end);
-		//#pragma omp for collapse(2) ordered
-				// firstprivate(i_start, j_start, i_end, j_end)
-		//dzieli petle w taki sposob, ze np zielony nie moze w ogole sie wykonac
-		for(int i = 0; i < SIZE; i++)
+		#ifndef NESTED
 		{
-			for(int j = 0; j < SIZE; j++)
+			for(int i = i_start; i < i_end; i++)
 			{
-				#ifdef _COUNT_LOOPS_
-				#pragma omp atomic
-				atomic_cnt++;
-				#endif
-			
-				// printf("ptid= %d i=%d j=%d\n",ptid, i,j);
-				// printf("is=%d js=%d\n",i_start,j_start);
-				// printf("ie=%d je=%d\n\n",i_end,j_end);
-
-
-				if(i <= i_end && i >= i_start &&
-					j <= j_end && j >= j_start)
+				for(int j = j_start; j < j_end; j++)
 				{
-					ppm_arr[i][j] = ptid + 1;
-					if(isprime(ulam_get_map(i,j, SIZE), &values[i][j]))
+					#ifdef _COUNT_LOOPS_
+					#pragma omp atomic
+					atomic_cnt++;
+					#endif
+				
+					// printf("ptid= %d i=%d j=%d\n",ptid, i,j);
+					// printf("is=%d js=%d\n",i_start,j_start);
+					// printf("ie=%d je=%d\n\n",i_end,j_end);
+
+
+					if(i <= i_end && i >= i_start &&
+						j <= j_end && j >= j_start)
 					{
-						
-						ppm_arr[i][j] = 0;
+						ppm_arr[i][j] = ptid + 1;
+						if(isprime(ulam_get_map(i,j, SIZE), &values[i][j]))
+						{
+							
+							ppm_arr[i][j] = 0;
+						}
 					}
 				}
 			}
 		}
+		#endif
+		#ifdef NESTED
+		{
+			int block = BLOCK_SIZE(SIZE);
+			#pragma omp parallel\
+					num_threads(4)\
+					firstprivate(ptid, i_start, i_end, j_start, j_end)\
+					shared(block)
+			{
+				int ctid = omp_get_thread_num();
+				int ci_start, ci_end, cj_start, cj_end;
+
+				#pragma omp critical
+				{
+					#ifdef _COUNT_LOOPS_
+					#pragma omp atomic
+					atomic_cnt++;
+					#endif
+					
+					//eg		i 400 800; j 400 800
+					//ci		400 600	400	600
+					//ci_end	600	800	600	800
+					//cj		400	400	600	600
+					//cj_end	600	600	800	800
+
+					ci_start = i_start + (ctid % 2) * BLOCK_SIZE(block);
+					ci_end = ci_start + BLOCK_SIZE(block);
+
+					cj_start = j_start + (ctid/2) * BLOCK_SIZE(block);
+					cj_end = cj_start + BLOCK_SIZE(block);
+
+					printf("ptid = %d\t ctid = %d\n", ptid, ctid);
+					printf("cis=%d cie=%d\tcjs=%d cje=%d\n",
+							ci_start, ci_end, cj_start, cj_end);
+					printf("is=%d ie=%d\tjs=%d je=%d\n\n",
+							i_start, i_end, j_start, j_end);
+				}
+				for(int i = ci_start; i < ci_end; i++)
+				{
+					for(int j = cj_start; j < cj_end; j++)
+					{
+						#ifdef _COUNT_LOOPS_
+						#pragma omp atomic
+						atomic_cnt++;
+						#endif
+
+						if(i <= ci_end && i >= ci_start &&
+							j <= cj_end && j >= cj_start)
+						{
+							ppm_arr[i][j] = ctid + ptid * no_threads + 1;
+							if(isprime(ulam_get_map(i,j, SIZE), &values[i][j]))
+							{
+								ppm_arr[i][j] = 0;
+							}
+						}
+					}
+				}
+			}
+		}
+		#endif
 	}
+
 	#ifdef _COUNT_LOOPS_
 	printf("atomic_cnt = %d\n", atomic_cnt);
-	ASSERT(atomic_cnt == )
+	//assert(atomic_cnt == SIZE * SIZE * 4);
 	#endif
 
 	end_time = omp_get_wtime();
